d5b7af4aa8c404648a7777943200162c50ae7df9
ChrisCarcamo1605 <carcamox1234@gmail.com>
2025-04-05 11:21:28 -0600

    Dtos modified and Put request added to ProductController

--- src/main/java/com/unicaes/poo/domain/products/dto/DtoProductsList.java
+++ src/main/java/com/unicaes/poo/domain/products/dto/DtoProductsList.java
@@ -6,11 +6,11 @@ import com.unicaes.poo.domain.products.Product;
 
 import java.math.BigDecimal;
 
-public record DtoProductsList(String name, BigDecimal priceCost, BigDecimal priceSell, String description
+public record DtoProductsList(Long id, String name, BigDecimal priceCost, BigDecimal priceSell, String description
         , MeasurementUnit measurementUnit) {
 
     public DtoProductsList(Product product) {
-        this(product.getName(),
+        this( product.getId(), product.getName(),
                 product.getPriceCost(),
                 product.getPriceSell(),
                 product.getDescription(),
